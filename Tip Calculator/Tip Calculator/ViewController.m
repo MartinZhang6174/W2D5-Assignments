13002faa91e5f81130f6fae30867af30c1004320
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-08 13:08:37 -0700

    Done

--- Tip Calculator/Tip Calculator/ViewController.m
+++ Tip Calculator/Tip Calculator/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (strong, nonatomic) IBOutlet UILabel *tipAmountDisplayLabel;
+@property (strong, nonatomic) IBOutlet UITextField *tipRateInput;
 
 @end
 
@@ -24,4 +27,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tappedEndEditing:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)calculateTip:(id)sender {
+    float billAmountFloatValue = [self.billAmountTextField.text floatValue];
+    float hundred = 100.00;
+    NSString *percentageSymbol = @"%";
+    if (self.tipRateInput != nil) {
+        // If the program receives a userinput for tip rate specification:
+        float userInputTipRate = [self.tipRateInput.text floatValue];
+
+        float userInputRateInPercentage = userInputTipRate / hundred;
+        float properTip = billAmountFloatValue * userInputRateInPercentage;
+        self.tipAmountDisplayLabel.text = [NSString stringWithFormat:@"Your total bill is $%.02f, your desired tip rate is %.0f%@ and the calculated tip is $%.02f!", billAmountFloatValue, userInputTipRate,percentageSymbol, properTip];
+    } else {
+        // If the user does not specify the rate of the tip:
+        float tipRate = 0.15;
+        float properTip = billAmountFloatValue * tipRate;
+        // Converted precise tip fload into an NSString with 2 decimal precisions.
+        
+        self.tipAmountDisplayLabel.text = [NSString stringWithFormat:@"Your total bill is $%.02f, and the suggested tip is $%.02f!", billAmountFloatValue, properTip];
+    }
+
+}
+
 @end
+
+
+
+
+
+
+
+
+
