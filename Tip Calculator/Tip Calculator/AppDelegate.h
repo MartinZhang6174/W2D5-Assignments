e93cf7a28b5dd8dc3ec3d6e681c5c62cb631d1dd
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-07-08 11:20:14 -0700

    Created calculator

---
+++ Tip Calculator/Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Martin Zhang on 2016-07-08.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
